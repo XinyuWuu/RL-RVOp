9e0b39c3e0a162b22680b815c367aa2bd9c777a4
Xinyu-Wu-0000 <xinyu.wuxinyu@outlook.com>
2023-03-07 22:19:12 +0800

    add vf_end

--- videoIO.py
+++ videoIO.py
@@ -4,10 +4,10 @@ from time import strftime, localtime
 
 
 class VideoIO():
-    def __init__(self, vf_name="", framerate=50, codec="hevc", w=1920, h=1080, pix_fmt="yuv420p") -> None:
+    def __init__(self, vf_name="", framerate=50, codec="hevc", w=1920, h=1080, pix_fmt="yuv420p", vf_end="glfw") -> None:
         if vf_name == "":
             self.vf_name = strftime(
-                "./assets/%H:%M_%d_%m_glfw.mp4", localtime())
+                f"./assets/%H:%M_%d_%m_{vf_end}.mp4", localtime())
             pass
         else:
             self.vf_name = vf_name
