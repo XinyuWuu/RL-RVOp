8c8cd27e25ecf540fa34a85e3aba38dd00d4ad7f
Xinyu-Wu-0000 <xinyu.wuxinyu@outlook.com>
2023-03-21 19:49:51 +0800

    move d dpre to module

--- nornntest.py
+++ nornntest.py
@@ -28,10 +28,38 @@ import random
 import nornnsac
 import importlib
 
-seed = 16
-torch.manual_seed(seed)
-np.random.seed(seed)
-random.seed(seed)
+PARAMs = {
+    "seed": 0,
+    "isdraw": True,
+    "isrender": True,
+    "codec": 'h264',
+    "framerate": 10,
+    "dreach": 0.02,
+    "rreach": 30.0,
+    "dmax": 3.0,
+    "vmax": 1.0,
+    "tau": 0.5,
+    "device": "cuda",
+    "gamma": 0.99,
+    "polyak": 0.995,
+    "lr": 5e-4,
+    "alpha": 0.005,
+    "act_dim": 2,
+    "max_obs": 16,
+    "obs_self_dim": 4,
+    "obs_sur_dim": 10,
+    "hidden_sizes": [1024, 1024, 1024],
+    "replay_size": int(1e6),
+}
+PARAMs["obs_dim"] = PARAMs["obs_self_dim"] + \
+    PARAMs["max_obs"] * (PARAMs["obs_sur_dim"] + 1)
+PARAMs["act_limit"] = np.array(
+    [PARAMs["vmax"], PARAMs["vmax"]], dtype=np.float32)
+
+
+torch.manual_seed(PARAMs["seed"])
+np.random.seed(PARAMs["seed"])
+random.seed(PARAMs["seed"])
 
 importlib.reload(nornnsac)
 importlib.reload(envCreator)
@@ -44,37 +72,21 @@ importlib.reload(simulator_cpp)
 torch.set_num_threads(torch.get_num_threads())
 
 # config environment
-isdraw = True
-isrender = True
-codec = 'h264'
-framerate = 10
-dreach = 0.02
-rreach = 30
-dmax = 3.0
-vmax = 1.0
-tau = 0.5
-CCcpp = CtrlConverter(vmax=vmax, tau=tau)
-rmax = CCcpp.get_rmax()
-SMLT = simulator_cpp.Simulator(dmax=dmax, framerate=framerate, dreach=dreach)
-SMLT.set_reward(vmax=vmax, rmax=rmax, tolerance=0.015,
-                a=4.0, b=0.5, c=2, d=0.5, e=0.5, f=4, g=0.1, eta=0.125, h=0.15, mu=0.375)
+CCcpp = CtrlConverter(vmax=PARAMs["vmax"], tau=PARAMs["tau"])
+PARAMs["rmax"] = CCcpp.get_rmax()
+SMLT = simulator_cpp.Simulator(
+    dmax=PARAMs["dmax"], framerate=PARAMs["framerate"], dreach=PARAMs["dreach"])
+SMLT.set_reward(vmax=PARAMs["vmax"], rmax=PARAMs["rmax"], tolerance=0.015,
+                a=4.0, b=0.5, c=2, d=0.5, e=0.5, f=4, g=0.1, eta=0.125, h=0.15, mu=0.375, rreach=PARAMs["rreach"])
 font = ImageFont.truetype(
     "/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf", 25)
 # cofig SAC
-device = "cuda"
-act_dim = 2
-max_obs = 16
-obs_self_dim = 4
-obs_sur_dim = 10
-obs_dim = obs_self_dim + max_obs * (obs_sur_dim + 1)
-act_limit = np.array([vmax, vmax], dtype=np.float32)
-hidden_sizes = [1024, 1024, 1024]
-
-Pi = nornnsac.nornncore.Policy(obs_dim, act_dim, act_limit, hidden_sizes)
-# Pi.load_state_dict(torch.load(
-#     "module_saves/max_67.34_4robots_0h_7min_6899steps_policy.ptd"))
-Pi.to(device)
-Pi.act_limit = Pi.act_limit.to(device)
+Pi = nornnsac.nornncore.Policy(obs_dim=PARAMs["obs_dim"], act_dim=PARAMs["act_dim"],
+                               act_limit=PARAMs["act_limit"], hidden_sizes=PARAMs["hidden_sizes"])
+Pi.load_state_dict(torch.load(
+    "module_saves/2h_12min_65999steps_policy.ptd"))
+Pi.to(device=PARAMs["device"])
+Pi.act_limit = Pi.act_limit.to(device=PARAMs["device"])
 
 
 def preNNinput(NNinput: tuple, obs_sur_dim: int, max_obs: int, device):
@@ -95,41 +107,34 @@ def preNNinput(NNinput: tuple, obs_sur_dim: int, max_obs: int, device):
 ###########################################################
 # init environment get initial observation
 # init model
-Nrobot = 11
-# robot_text = SMLT.EC.circle_robot(Nrobot)
-# obs_text1, obs1 = SMLT.EC.circle_obstacle(11, 'l')
-# obs_text2, obs2 = SMLT.EC.circle_obstacle(3, 's')
-# pos_vel, observation, r, NNinput, d = SMLT.set_model(
-#     Nrobot, robot_text, obs_text1 + obs_text2, obs1 + obs2, "circle")
-
-robot_text = SMLT.EC.line_robot(Nrobot)
-obs_text1, obs1 = SMLT.EC.gate_obstacle(True)
-pos_vel, observation, r, NNinput, d = SMLT.set_model(
-    Nrobot, robot_text, obs_text1, obs1, "circle")
-SMLT.EC.actuator(Nrobot)
-text = SMLT.EC.env_text(robot_text, obs_text1, SMLT.EC.actuator(Nrobot))
-with open("assets/test.xml", 'w') as fp:
-    fp.write(text)
-o = preNNinput(NNinput, obs_sur_dim, max_obs, device)
+Nrobot = 4
+robot_text = SMLT.EC.circle_robot(Nrobot)
+obs_text1, obs1 = SMLT.EC.circle_obstacle(3, 'l')
+obs_text2, obs2 = SMLT.EC.circle_obstacle(1, 's')
+pos_vel, observation, r, NNinput, d, dpre = SMLT.set_model(
+    Nrobot, robot_text, obs_text1 + obs_text2, obs1 + obs2, "circle")
+o = preNNinput(NNinput, PARAMs["obs_sur_dim"],
+               PARAMs["max_obs"], PARAMs["device"])
 ep_ret = 0
 ep_len = 0
 
 # config training process
 max_simu_second = 30
-max_ep_len = int(max_simu_second * framerate)
+max_ep_len = int(max_simu_second * PARAMs["framerate"])
 num_test_episodes = 10
 total_steps = max_ep_len * num_test_episodes
 
-if isrender:
+if PARAMs["isrender"]:
     RD = render.Render()
     RD.set_model(SMLT.mjMODEL, SMLT.mjDATA)
     RD.switchCam()
-    videofp = videoIO.VideoIO("assets/video.mp4", SMLT.framerate, codec=codec)
+    videofp = videoIO.VideoIO(
+        "assets/video.mp4", SMLT.framerate, codec=PARAMs["codec"])
 
-if isdraw:
+if PARAMs["isdraw"]:
     CV = canvas.Canvas(w=16, h=16)
     canvasfp = videoIO.VideoIO(
-        "assets/video_canvas.mp4", SMLT.framerate, codec=codec, w=CV.w * CV.dpi, h=CV.h * CV.dpi)
+        "assets/video_canvas.mp4", SMLT.framerate, codec=PARAMs["codec"], w=CV.w * CV.dpi, h=CV.h * CV.dpi)
 
 
 # Main loop: collect experience in env and update/log each epoch
@@ -139,44 +144,37 @@ for t in range(total_steps):
     a = a.cpu().detach().numpy()
 
     # Step the env
-    aglobal = a
+    aglobal = a.copy()
     onumpy = o.cpu().detach().numpy()
     for Nth in range(SMLT.Nrobot):
         aglobal[Nth] = np.matmul(
             np.array([[np.cos(pos_vel[Nth][2]), -np.sin(pos_vel[Nth][2])],
                       [np.sin(pos_vel[Nth][2]), np.cos(pos_vel[Nth][2])]]),
-            aglobal[Nth] + onumpy[Nth][0:2] / norm(onumpy[Nth][0:2])
+            aglobal[Nth]
         )
         # aglobal[Nth] = np.matmul(
         #     np.array([[np.cos(pos_vel[Nth][2]), -np.sin(pos_vel[Nth][2])],
         #               [np.sin(pos_vel[Nth][2]), np.cos(pos_vel[Nth][2])]]),
         #     onumpy[Nth][0:2] / norm(onumpy[Nth][0:2])
         # )
-    ctrl=CCcpp.v2ctrlbatch(posvels=pos_vel, vs=aglobal)
-    for Nth in range(SMLT.Nrobot):
-        if d[Nth] == 1:
-            ctrl[Nth * 2: Nth * 2 + 2]=[0, 0]
-    dpre=d
-    pos_vel, observation, r, NNinput, d=SMLT.step(ctrl)
-    d=array([1 if dpre[i] == 1 or d[i] ==
-              1 else 0 for i in range(d.shape[0])])
-    r=array([r[rNth] + rreach if d[rNth] == 1 and dpre[rNth] ==
-              0 else r[rNth] for rNth in range(r.__len__())], dtype=np.float32)
-    o2=preNNinput(NNinput, obs_sur_dim, max_obs, device)
+    ctrl = CCcpp.v2ctrlbatch(posvels=pos_vel, vs=aglobal)
+    pos_vel, observation, r, NNinput, d, dpre = SMLT.step(ctrl)
+    o2 = preNNinput(NNinput, PARAMs["obs_sur_dim"],
+                    PARAMs["max_obs"], PARAMs["device"])
     ep_ret += r.mean()
     ep_len += 1
 
-    if isrender:
+    if PARAMs["isrender"]:
         RD.render()
         videofp.write_frame(flipud(RD.rgb))
-    if isdraw:
+    if PARAMs["isdraw"]:
         CV.newCanvas()
         CV.draw_contour(SMLT.contours)
         # print(o2)
-        onumpy=o2.cpu().detach().numpy()
+        onumpy = o2.cpu().detach().numpy()
         for i in range(SMLT.Nrobot):
-            oi=onumpy[i]
-            tranM=np.array([[np.cos(pos_vel[i][2]), -np.sin(pos_vel[i][2])],
+            oi = onumpy[i]
+            tranM = np.array([[np.cos(pos_vel[i][2]), -np.sin(pos_vel[i][2])],
                               [np.sin(pos_vel[i][2]), np.cos(pos_vel[i][2])]])
             # draw reward
             CV.draw_text(pos_vel[i][0:2], f"{r[i]:.2f}", font=font)
@@ -184,17 +182,17 @@ for t in range(total_steps):
             CV.draw_line(pos_vel[i][0:2], pos_vel[i]
                          [0:2] + pos_vel[i][3:5], "purple", 2)
             # draw action
-            # CV.draw_line(pos_vel[i][0:2], pos_vel[i]
-            #              [0:2] + aglobal[i], "green", 2)
+            CV.draw_line(pos_vel[i][0:2], pos_vel[i]
+                         [0:2] + aglobal[i], "green", 2)
             # draw target
             # CV.draw_line(pos_vel[i][0:2], np.matmul(
             #     tranM, oi[0:2]) + pos_vel[i][0:2])
-            for o in range(observation[i].__len__()):
-                o2draw=oi[5 + o * 11:5 + o * 11 + 8]
-                o2draw[0:2]=np.matmul(tranM, o2draw[0:2])
-                o2draw[2:4]=np.matmul(tranM, o2draw[2:4])
-                o2draw[4:6]=np.matmul(tranM, o2draw[4:6])
-                o2draw[6:8]=np.matmul(tranM, o2draw[6:8])
+            for o in range(min(observation[i].__len__(),PARAMs["max_obs"])):
+                o2draw = oi[5 + o * 11:5 + o * 11 + 8]
+                o2draw[0:2] = np.matmul(tranM, o2draw[0:2])
+                o2draw[2:4] = np.matmul(tranM, o2draw[2:4])
+                o2draw[4:6] = np.matmul(tranM, o2draw[4:6])
+                o2draw[6:8] = np.matmul(tranM, o2draw[6:8])
                 CV.draw_rvop(o2draw, pos_vel[i][0:2])
         for pos in pos_vel:
             CV.draw_dmax(pos[0:2], SMLT.dmax)
@@ -205,7 +203,7 @@ for t in range(total_steps):
 
     # Super critical, easy to overlook step: make sure to update
     # most recent observation!
-    o=o2
+    o = o2
 
     # End of trajectory handling
     if (d == 1).all() or (ep_len == max_ep_len):
@@ -213,43 +211,45 @@ for t in range(total_steps):
             f"t: {t}, {Nrobot} robots, ep_ret: {ep_ret:.2f}, ep_len: {ep_len}, Nreach: {d.sum()}")
         # TODO change environment according to t
         if t < max_ep_len * 1:
-            Nrobot=1
-            robot_text=SMLT.EC.circle_robot(Nrobot)
-            obs_text1, obs1=SMLT.EC.circle_obstacle(3, 'l')
-            obs_text2, obs2=SMLT.EC.circle_obstacle(1, 's')
+            Nrobot = 4
+            robot_text = SMLT.EC.circle_robot(Nrobot)
+            obs_text1, obs1 = SMLT.EC.circle_obstacle(3, 'l')
+            obs_text2, obs2 = SMLT.EC.circle_obstacle(1, 's')
         elif t < max_ep_len * 4:
-            Nrobot=4
-            robot_text=SMLT.EC.circle_robot(Nrobot)
-            obs_text1, obs1=SMLT.EC.circle_obstacle(6, 'l')
-            obs_text2, obs2=SMLT.EC.circle_obstacle(2, 's')
+            Nrobot = 4
+            robot_text = SMLT.EC.circle_robot(Nrobot)
+            obs_text1, obs1 = SMLT.EC.circle_obstacle(6, 'l')
+            obs_text2, obs2 = SMLT.EC.circle_obstacle(2, 's')
         elif t < max_ep_len * 8:
-            Nrobot=8
-            robot_text=SMLT.EC.circle_robot(Nrobot)
-            obs_text1, obs1=SMLT.EC.circle_obstacle(8, 'l')
-            obs_text2, obs2=SMLT.EC.circle_obstacle(3, 's')
+            Nrobot = 8
+            robot_text = SMLT.EC.circle_robot(Nrobot)
+            obs_text1, obs1 = SMLT.EC.circle_obstacle(8, 'l')
+            obs_text2, obs2 = SMLT.EC.circle_obstacle(3, 's')
         else:
-            Nrobot=12
-            robot_text=SMLT.EC.circle_robot(16)
-            robot_text += SMLT.EC.circle_robot(6, 's', 16)
-            obs_text1, obs1=SMLT.EC.circle_obstacle(8, 'l')
-            obs_text2, obs2=SMLT.EC.circle_obstacle(3, 's')
+            Nrobot = 12
+            robot_text = SMLT.EC.circle_robot(8)
+            robot_text += SMLT.EC.circle_robot(4, 's', 8)
+            obs_text1, obs1 = SMLT.EC.circle_obstacle(8, 'l')
+            obs_text2, obs2 = SMLT.EC.circle_obstacle(3, 's')
 
-        pos_vel, observation, r, NNinput, d=SMLT.set_model(Nrobot, robot_text, obs_text1 +
-                                                             obs_text2, obs1 + obs2, "circle")
-        o=preNNinput(NNinput, obs_sur_dim, max_obs, device)
-        ep_ret=0
-        ep_len=0
+        pos_vel, observation, r, NNinput, d, dpre = SMLT.set_model(Nrobot, robot_text, obs_text1 +
+                                                                   obs_text2, obs1 + obs2, "circle")
+        o = preNNinput(NNinput, PARAMs["obs_sur_dim"],
+                        PARAMs["max_obs"], PARAMs["device"])
+        ep_ret = 0
+        ep_len = 0
 
-        if isdraw:
+        if PARAMs["isdraw"]:
             canvasfp.close()
-        if isrender:
+        if PARAMs["isrender"]:
             videofp.close()
 
-        if isrender:
+        if PARAMs["isrender"]:
             RD.set_model(SMLT.mjMODEL, SMLT.mjDATA)
             RD.switchCam()
-            videofp=videoIO.VideoIO("", SMLT.framerate, codec=codec)
+            videofp = videoIO.VideoIO(
+                "", SMLT.framerate, codec=PARAMs["codec"])
 
-        if isdraw:
-            canvasfp=videoIO.VideoIO(
-                "", SMLT.framerate, codec=codec, w=CV.w * CV.dpi, h=CV.h * CV.dpi, vf_end="draw")
+        if PARAMs["isdraw"]:
+            canvasfp = videoIO.VideoIO(
+                "", SMLT.framerate, codec=PARAMs["codec"], w=CV.w * CV.dpi, h=CV.h * CV.dpi, vf_end="draw")
--- nornntrain.py
+++ nornntrain.py
@@ -33,7 +33,7 @@ PARAMs = {
     "codec": 'h264',
     "framerate": 10,
     "dreach": 0.02,
-    "rreach": 30,
+    "rreach": 30.0,
     "dmax": 3.0,
     "vmax": 1.0,
     "tau": 0.5,
@@ -47,7 +47,7 @@ PARAMs = {
     "obs_self_dim": 4,
     "obs_sur_dim": 10,
     "hidden_sizes": [1024, 1024, 1024],
-    "replay_size" : int(1e6),
+    "replay_size": int(1e6),
 }
 PARAMs["obs_dim"] = PARAMs["obs_self_dim"] + \
     PARAMs["max_obs"] * (PARAMs["obs_sur_dim"] + 1)
@@ -70,67 +70,20 @@ importlib.reload(simulator_cpp)
 torch.set_num_threads(torch.get_num_threads())
 
 # config environment
-# isdraw = False
-# isrender = False
-# codec = 'h264'
-# framerate = 10
-# dreach = 0.02
-# rreach = 30
-# dmax = 3.0
-# vmax = 1.0
-# tau = 0.5
 CCcpp = CtrlConverter(vmax=PARAMs["vmax"], tau=PARAMs["tau"])
 PARAMs["rmax"] = CCcpp.get_rmax()
 SMLT = simulator_cpp.Simulator(
     dmax=PARAMs["dmax"], framerate=PARAMs["framerate"], dreach=PARAMs["dreach"])
 SMLT.set_reward(vmax=PARAMs["vmax"], rmax=PARAMs["rmax"], tolerance=0.015,
-                a=4.0, b=0.5, c=2, d=0.5, e=0.5, f=4, g=0.1, eta=0.125, h=0.15, mu=0.375)
+                a=4.0, b=0.5, c=2, d=0.5, e=0.5, f=4, g=0.1, eta=0.125, h=0.15, mu=0.375, rreach=PARAMs["rreach"])
 
 # cofig SAC
-# device = "cuda"
-# gamma = 0.99
-# polyak = 0.995
-# lr = 5e-4
-# alpha = 0.005
-# act_dim = 2
-# max_obs = 16
-# obs_self_dim = 4
-# obs_sur_dim = 10
-# obs_dim = obs_self_dim + max_obs * (obs_sur_dim + 1)
-# act_limit = np.array([vmax, vmax], dtype=np.float32)
-# hidden_sizes = [1024, 1024, 1024]
 SAC = nornnsac.SAC(obs_dim=PARAMs["obs_dim"], act_dim=PARAMs["act_dim"], act_limit=PARAMs["act_limit"],
                    hidden_sizes=PARAMs["hidden_sizes"], lr=PARAMs["lr"], gamma=PARAMs["gamma"], polyak=PARAMs["polyak"], alpha=PARAMs["alpha"], device=PARAMs["device"])
 
 # config replay buffer
 replay_buffer = nornnsac.nornncore.ReplayBufferLite(
     obs_dim=PARAMs["obs_dim"], act_dim=PARAMs["act_dim"], max_size=PARAMs["replay_size"])
-
-# def get_action(o, deterministic=False):
-#     with torch.no_grad():
-#         a, _ = Pi(torch.as_tensor(o, dtype=torch.float32).to(
-#             device), deterministic, False)
-#         return a.cpu().detach().numpy()
-
-# def test_agent():
-#     total_ret, total_len, succ_rate = 0, 0, 0
-#     for j in range(num_test_episodes):
-#         o, d, ep_ret, ep_len, succ, = test_env.reset(), False, 0, 0, False
-#         while not (d or (ep_len == max_ep_len)):
-#             # Take deterministic actions at test time
-#             a = get_action(o, True)
-#             o, r, d, truncated, info = test_env.step(a)
-#             ep_ret += r
-#             ep_len += 1
-#             total_ret += r
-#             total_len += 1
-#             if d == 1 and r > 0:
-#                 succ = True
-#                 succ_rate += 1
-#         print(
-#             f"test result: ret: {ep_ret:.2f}, len: {ep_len}, success: {succ}")
-
-#     return total_ret / num_test_episodes, total_len / num_test_episodes, succ_rate / num_test_episodes
 
 
 def preNNinput(NNinput: tuple, obs_sur_dim: int, max_obs: int, device):
@@ -155,9 +108,10 @@ Nrobot = 4
 robot_text = SMLT.EC.circle_robot(Nrobot)
 obs_text1, obs1 = SMLT.EC.circle_obstacle(3, 'l')
 obs_text2, obs2 = SMLT.EC.circle_obstacle(1, 's')
-pos_vel, observation, r, NNinput, d = SMLT.set_model(Nrobot, robot_text, obs_text1 +
-                                                     obs_text2, obs1 + obs2, "circle")
-o = preNNinput(NNinput, PARAMs["obs_sur_dim"], PARAMs["max_obs"], PARAMs["device"])
+pos_vel, observation, r, NNinput, d, dpre = SMLT.set_model(Nrobot, robot_text, obs_text1 +
+                                                           obs_text2, obs1 + obs2, "circle")
+o = preNNinput(NNinput, PARAMs["obs_sur_dim"],
+               PARAMs["max_obs"], PARAMs["device"])
 ep_ret = 0
 ep_len = 0
 
@@ -184,16 +138,16 @@ NN_update_count = 0
 for t in range(total_steps):
 
     if t > random_steps:
-        # TODO only d == 0
         with torch.no_grad():
             a, logp = SAC.Pi(o, with_logprob=False)
             a = a.cpu().detach().numpy()
     else:
-        a = (np.random.rand(Nrobot, PARAMs["act_dim"]) * 2 - 1) * PARAMs["act_limit"]
+        a = (np.random.rand(
+            Nrobot, PARAMs["act_dim"]) * 2 - 1) * PARAMs["act_limit"]
 
     # Step the env
     timebegin = time.time()
-    aglobal = a
+    aglobal = a.copy()
     for Nth in range(SMLT.Nrobot):
         aglobal[Nth] = np.matmul(
             np.array([[np.cos(pos_vel[Nth][2]), -np.sin(pos_vel[Nth][2])],
@@ -201,17 +155,11 @@ for t in range(total_steps):
             aglobal[Nth]
         )
     ctrl = CCcpp.v2ctrlbatch(posvels=pos_vel, vs=aglobal)
-    for Nth in range(SMLT.Nrobot):
-        if d[Nth] == 1:
-            ctrl[Nth * 2: Nth * 2 + 2] = [0, 0]
-    dpre = d
-    pos_vel, observation, r, NNinput, d = SMLT.step(ctrl)
+
+    pos_vel, observation, r, NNinput, d, dpre = SMLT.step(ctrl)
     time_for_step += time.time() - timebegin
-    d = array([1 if dpre[i] == 1 or d[i] ==
-              1 else 0 for i in range(d.shape[0])])
-    r = array([r[rNth] + PARAMs["rreach"] if d[rNth] == 1 and dpre[rNth] ==
-              0 else r[rNth] for rNth in range(r.__len__())], dtype=np.float32)
-    o2 = preNNinput(NNinput, PARAMs["obs_sur_dim"], PARAMs["max_obs"], PARAMs["device"])
+    o2 = preNNinput(NNinput, PARAMs["obs_sur_dim"],
+                    PARAMs["max_obs"], PARAMs["device"])
     ep_ret += r.mean()
     ep_len += 1
 
@@ -235,7 +183,6 @@ for t in range(total_steps):
             torch.save(SAC.Pi.state_dict(),
                        f'module_saves/max_{ep_ret:.2f}_{Nrobot}robots_{time_prefix}_policy.ptd')
 
-        # TODO change environment according to t
         if (t - random_steps) < max_ep_len * 80:
             Nrobot = 4
             robot_text = SMLT.EC.circle_robot(Nrobot)
@@ -246,7 +193,7 @@ for t in range(total_steps):
             robot_text = SMLT.EC.circle_robot(Nrobot)
             obs_text1, obs1 = SMLT.EC.circle_obstacle(6, 'l')
             obs_text2, obs2 = SMLT.EC.circle_obstacle(2, 's')
-        elif (t - random_steps) < max_ep_len * 4800:
+        elif (t - random_steps) < max_ep_len * 480:
             Nrobot = 12
             robot_text = SMLT.EC.circle_robot(Nrobot)
             obs_text1, obs1 = SMLT.EC.circle_obstacle(8, 'l')
@@ -258,8 +205,8 @@ for t in range(total_steps):
             obs_text1, obs1 = SMLT.EC.circle_obstacle(8, 'l')
             obs_text2, obs2 = SMLT.EC.circle_obstacle(3, 's')
 
-        pos_vel, observation, r, NNinput, d = SMLT.set_model(Nrobot, robot_text, obs_text1 +
-                                                             obs_text2, obs1 + obs2, "circle")
+        pos_vel, observation, r, NNinput, d, dpre = SMLT.set_model(Nrobot, robot_text, obs_text1 +
+                                                                   obs_text2, obs1 + obs2, "circle")
         o = preNNinput(NNinput, PARAMs["obs_sur_dim"],
                        PARAMs["max_obs"], PARAMs["device"])
         ep_ret = 0
@@ -272,10 +219,11 @@ for t in range(total_steps):
         lossq_log = np.zeros(update_num)
         alpha_log = np.zeros(update_num)
         timebegin = time.time()
+
         for j in range(update_num):
             batch = replay_buffer.sample_batch(batch_size, PARAMs["device"])
             losspi_log[j], lossq_log[j], alpha_log[j] = SAC.update(data=batch)
-            # alpha = np.exp(log_alpha.cpu().detach().numpy())
+
         timeend = time.time()
         time_for_NN_update += timeend - timebegin
         print(
@@ -288,46 +236,6 @@ for t in range(total_steps):
     # End of epoch handling
     if (t + 1) % steps_per_epoch == 0:
         epoch = (t + 1) // steps_per_epoch
-
-        # Test the performance of the deterministic version of the agent.
-        # print("!!!!!!!!!!!!!!!!test!!!!!!!!!!!!!!!!!")
-        # ave_ret, ave_len, succ_rate = test_agent()
-        # print(
-        #     f"test result: ret: {ave_ret:.2f}, len: {ave_len:.2f}, time: {((time.time() - start_time) / 3600):.2f}, alpha: {alpha:.2f}")
-        # if ave_ret > max_ret or succ_rate > 0.9:
-        #     max_ret = ave_ret
-        #     max_ret_time = time.time()
-        #     max_ret_rel_time = (time.time() - start_time) / 3600
         save_prefix = f"{int((time.time()-start_time)/3600)}h_{int((int(time.time()-start_time)%3600)/60)}min_{t}steps"
         torch.save(SAC.Pi.state_dict(),
                    f'module_saves/{save_prefix}_policy.ptd')
-
-
-# env=simulator.Simulator(2, show_figure=False),
-# test_env=simulator.Simulator(2, show_figure=True),
-# obs_dim=8,
-# act_dim=2,
-# act_limit=np.array([0.2, 3.63]),
-# hidden_sizes=(256, 256, 256),
-# activation=torch.nn.ReLU,
-# device="cuda",
-# seed=0,
-# steps_per_epoch=6000,
-# epochs=1000,
-# replay_size=int(1e6),
-# gamma=0.99,
-# polyak=0.995,
-# lr=1e-3,
-# alpha=0.005,
-# batch_size=256,
-# random_steps=10000,
-# update_after=1000,
-# update_every=50,
-# num_test_episodes=1,
-# max_ep_len=3000
-
-# a = torch.tensor(np.arange(100).reshape(-1, 2)).to('cuda')
-# b = torch.tensor(np.array([2, 1])).to('cuda')
-# a / b
-# torch.broadcast_to(b, [a.shape[0], b.shape[0]])
-# torch.hstack([a, torch.broadcast_to(b, [a.shape[0], b.shape[0]])])
